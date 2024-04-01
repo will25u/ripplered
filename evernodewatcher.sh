fa463394259144645f7bc2044971f3e9064efcea
will25u <32363823+will25u@users.noreply.github.com>
2024-04-01 17:38:56 -0400

    Update evernodewatcher.sh

--- evernodewatcher.sh
+++ evernodewatcher.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
 # Directory to store files
-INSTALL_DIR="/opt/ripplered/"
-
-# Set up log file
-LOG_FILE="$INSTALL_DIR/evernodewatcher.log"
-touch "$LOG_FILE" || { echo "Failed to create log file $LOG_FILE"; exit 1; }
-chmod 644 "$LOG_FILE" || { echo "Failed to set permissions for log file $LOG_FILE"; exit 1; }
-
-# Set up log rotation
-LOGROTATE_CONFIG="/etc/logrotate.d/evernodewatcher"
-cat << EOF | sudo tee "$LOGROTATE_CONFIG" > /dev/null || { echo "Failed to write logrotate config"; exit 1; }
-$LOG_FILE {
-    size 5k
-    rotate 1
-    notifempty
-    compress
-}
-EOF
+INSTALL_DIR="/opt/ripplered"
 
 # Notification URL file
 NOTIFICATION_URL_FILE="$INSTALL_DIR/notification_url.txt"
@@ -46,9 +30,4 @@ if ! curl -s "$NOTIFICATION_URL" >/dev/null; then
     exit 1
 fi
 
-# Make the script executable
-chmod +x "$INSTALL_DIR/evernodewatcher.sh"
-
-# Set up cron job
-CRON_JOB="* * * * * $INSTALL_DIR/evernodewatcher.sh >> $LOG_FILE 2>&1"
-(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+echo "Notification URL set up successfully."
